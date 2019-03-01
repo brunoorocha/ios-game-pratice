72b971822ba49bffe3de2ea77f8806123c6ad548
Bruno Rocha <mail.brunorocha@gmail.com>
2019-03-01 10:14:56 -0300

    Implementing attack combo

---
+++ spritekit-plataform/models/entities/Fighter/states/FighterAttack2State.swift
@@ -0,0 +1,40 @@
+//
+//  FighterAttack2State.swift
+//  spritekit-plataform
+//
+//  Created by Bruno Rocha on 01/03/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class FighterAttack2State: GKState {
+    var node: SKSpriteNode!
+    var stateAtlasTextures: [SKTexture] = []
+    
+    init(withNode node: SKSpriteNode) {
+        self.node = node
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+        switch stateClass {
+        case is FighterIdleState.Type:
+            return true
+        case is FighterWalkState.Type:
+            return true
+        case is FighterDieState.Type:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    override func didEnter(from previousState: GKState?) {
+        let attackAction = SKAction.animate(with: self.stateAtlasTextures, timePerFrame: 0.15, resize: true, restore: true)
+        node.run(attackAction, completion: {
+            self.stateMachine?.enter(FighterIdleState.self)
+        })
+    }
+}
+
